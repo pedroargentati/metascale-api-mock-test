2ca9c4127298ede34535477c88f666c868cbacc0
Pedro <rm88246@fiap.com.br>
2024-09-26 00:40:17 -0300

    style: removidos imports desnecessários

--- src/main/java/br/com/metascale/domain/entity/UserProduct.java
+++ src/main/java/br/com/metascale/domain/entity/UserProduct.java
@@ -7,8 +7,6 @@ import br.com.metascale.domain.UserProductsDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
